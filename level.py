b69b3f8a71584170054afbd8d74c10c45792f760
tfrum <119856929+tfrum@users.noreply.github.com>
2023-03-14 05:34:16 -0600

    Mouse input working without camera focus
    Will focus camera on player when I have the renderer integrated

--- level.py
+++ level.py
@@ -9,7 +9,7 @@ class Level:
         # grab the display surface called in __main__ so that the level object can draw to it
         self.display_surface = pygame.display.get_surface()
 
-        # create the sprite group
+        # create the sprite group. Sprite groups are a part of pygame
         self.all_sprites = pygame.sprite.Group()
 
         # here we call the setup method which creates the player
@@ -19,12 +19,15 @@ class Level:
     def setup(self):
         # create the player
         # we initialized player to take a position and a group in player.py
-        # I'm going to set it to be central to the screen at all times.
+        # I'm going to set it to be central to the screen at all times. This will change when we have movement.
         self.player = Player((SCREEN_WIDTH/2,SCREEN_HEIGHT/2), self.all_sprites)
 
     # run the initialized Level
     def run(self,dt):
+
         # fill the underlying surface with black
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+
+        # this update method gets passed to all the chldren of all_sprites (player, enemies, etc)
+        self.all_sprites.update(dt)
--- player.py
+++ player.py
@@ -5,17 +5,49 @@ from settings import *
 # this is because the player is a sprite, lmao
 class Player(pygame.sprite.Sprite):
 
-    # we're just going to pass the position (pos) and the group for now.
+    # initialize only position (pos) and the group for now
     def __init__(self, pos, group):
 
-        # super() is being used to initialize the parent/superclass Sprite which player inherits from.
+        # super() is being used to initialize the parent/superclass Sprite which player inherits from
         super().__init__(group)
-
-        # this is the image that the player will be using, defines height and width.
-        # nice
-        self.image = pygame.Surface((69,32))
+        self.image = pygame.Surface((28,69))
         self.image.fill ('blue')
-        
-        # this is the rect that the player will be using. These are used for detection, collision, etc.
-        # it gets cenetered on the position of the player.
+        # this is the rect that the player will be using. Used for collision etc
         self.rect = self.image.get_rect(center = pos)
+
+        # movement
+        # Vector2(x, y) doesn't need to take arguments because it defaults to o,o
+        self.direction = pygame.math.Vector2()
+        # set position to be the center of the player rect
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 100
+
+    # take input for the player object. This will be UDLR until it is mouse input
+    # this will need to be updated to not run if player has clicked on an entity
+    def input(self):
+        keys = pygame.key.get_pressed()
+        mouse = pygame.mouse.get_pressed()
+
+        # making player move with mouse
+        if mouse[0]:
+           # update the direction vector based on the mouse position
+           mouse_pos = pygame.mouse.get_pos()
+           self.direction = pygame.math.Vector2(mouse_pos) - pygame.math.Vector2(self.rect.center)
+           self.direction.normalize_ip()
+        else:
+           # reset the direction when mouse released to stop movement
+           self.direction = pygame.math.Vector2(0, 0)
+
+
+
+
+
+
+    def move(self,dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
+
+    # run the update method passed from all_sprites group
+    def update(self, dt):
+        self.input()
+        self.move(dt)
