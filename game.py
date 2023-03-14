efb1a3fd24aa495dabd6a5a97a3ce7b39df6cc3f
tfrum <119856929+tfrum@users.noreply.github.com>
2023-03-14 01:57:28 -0600

    Window, first iso2d
    Just setting up the initial isometric 2d rendering system.
    
    We're going to start with a single layer. I'm going to extend this to render adjacent chunks of tiles.
    
    We'll probably just end up with a little random loop that goes to a position and decides whether or not to select  a chunk. Idk, it's 2AM

---
+++ game.py
@@ -0,0 +1,40 @@
+import pygame, sys, time, random
+
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('tesgame')
+screen = pygame.display.set_mode((900, 900),0,32)
+display = pygame.Surface((400, 400))
+
+grass_img = pygame.image.load('Art/grasstile.png').convert()
+#Sets the color to treat as transparent in the background
+grass_img.set_colorkey((0, 0, 0))
+
+f = open('Maps/testmap.txt')
+map_data = [[int(c) for c in row] for row in f.read().split('\n')]
+f.close()
+
+while True:
+    display.fill((0,0,0))
+
+    for y, row in enumerate(map_data):
+        for x, tile in enumerate(row):
+            if tile:
+                #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
+                display.blit(grass_img, (200 + x * 10 - y * 10, 100 + x * 5 + y * 5))
+                #This snippet would be the start of rendering the next layer of blocks or assets.
+                if random.randint(0, 1):
+                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+    #This is how we exit the game.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    pygame.display.update()
+    time.sleep(1)
