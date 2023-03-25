8a28619fad954ed850c664197b8afa345a9abd33
tfrum <119856929+tfrum@users.noreply.github.com>
2023-03-24 20:19:58 -0600

    Removed junk

--- assets/tst.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="Isometric-SpriteSheet" tilewidth="32" tileheight="32" tilecount="60" columns="6">
- <image source="Isometric Tileset/Isometric-SpriteSheet.png" width="192" height="320"/>
-</tileset>
